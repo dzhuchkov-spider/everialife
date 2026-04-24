0ccd6cb437940e3324bb5d874424e40895ed56f4
Zhuchkov Spider <d.zhuchkov@spider.ru>
2026-04-24 15:32:07 +0300

    Main Page 5

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import logoWeb from '../assets/logo/Web.svg';
+import arrowDown from '../assets/icons/arrow down.svg';
 
 const MainPage: React.FC = () => {
   const [searchQuery, setSearchQuery] = useState('');
@@ -71,9 +72,7 @@ const MainPage: React.FC = () => {
                   {item}
                 </span>
                 {index > 0 && (
-                  <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none">
-                    <path d="M7 10l5 5 5-5z" fill="#666"/>
-                  </svg>
+                  <img src={arrowDown} alt="arrow down" className="w-4 h-4" style={{ filter: 'brightness(0.2)' }} />
                 )}
               </div>
             ))}
@@ -85,9 +84,7 @@ const MainPage: React.FC = () => {
               <span className="text-xs sm:text-sm text-[#4d4d4d] tracking-wide px-2 py-0.5">
                 по номеру
               </span>
-              <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none">
-                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" fill="#666"/>
-              </svg>
+              <img src={arrowDown} alt="arrow down" className="w-4 h-4" style={{ filter: 'brightness(0.2)' }} />
             </div>
             <svg className="w-5 sm:w-6 h-5 sm:h-6 flex-shrink-0" viewBox="0 0 24 24" fill="none">
               <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" fill="#666"/>
